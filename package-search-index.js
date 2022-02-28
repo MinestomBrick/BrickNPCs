fe33d4b0ea1872f93186f9e772716f8eb5b732cc
jorisguffens <joris.guffens@outlook.com>
2022-02-28 16:48:39 +0000

    Deploying to javadocs from @ MinestomBrick/BrickNPCs@f11206530de41ba9ecdf852a596b12862ed1a541 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.minestombrick.npcs.api"},{"l":"org.minestombrick.npcs.api.npc"},{"l":"org.minestombrick.npcs.api.trait"},{"l":"org.minestombrick.npcs.api.traits"}];updateSearchResults();
